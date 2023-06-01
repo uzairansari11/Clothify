f2574fb2cb41eee1b3cc5f3db5481ae20b39e98e
uzairansari11 <uzairans532@gmail.com>
2023-06-01 12:22:25 +0530

    signup functionality done

--- Frontend/pet_project/src/App.js
+++ Frontend/pet_project/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Navbar from "./components/navbar/Navbar";
 
 import Routing from "./routing/Routing";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routing />
     </div>
   );
